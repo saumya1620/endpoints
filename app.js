5a335b98c55300a296132e7e86d619d538a46bbe
Saumya Jain <122440288+saumya1620@users.noreply.github.com>
2025-06-05 11:09:43 +0530

    Update app.js

--- app.js
+++ app.js
@@ -10,5 +10,9 @@ app.use('/', statusRouter);
 app.get('/', (req, res) => {
   res.send('Endpoints are running');
 });
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server running on port ${PORT}`);
+});
 
 module.exports = app;
